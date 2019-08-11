b8be5562daa2e28eca23b7931c3d8c1560789424
David Waller <dwaller@apple.com>
2019-08-11 09:23:05 +0200

    minor cleanups

--- .storybook/selectFirstStoryAddon.js
+++ .storybook/selectFirstStoryAddon.js
@@ -1,13 +1,9 @@
 // from https://stackoverflow.com/questions/53724555/specify-default-page-for-storybook
 import addonAPI from '@storybook/addons'
-import {
-  STORY_CHANGED,
-  SET_STORIES,
-  STORIES_CONFIGURED,
-} from '@storybook/core-events'
+import { STORIES_CONFIGURED } from '@storybook/core-events'
 
 let firstLoad = true
-console.log('initial')
+
 addonAPI.register('edumentab/firstStory', storybookAPI => {
   storybookAPI.on(STORIES_CONFIGURED, (kind, story) => {
     // when you enter a story, if you are just loading storybook up, default to a specific kind/story.
